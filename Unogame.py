bf5c29f4aaf7f5092a3e012d1f206c571daa224a
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2025-07-28 02:07:15 +0300

    building the wild functions

--- Unogame.py
+++ Unogame.py
@@ -4,12 +4,16 @@ def buildDeck():
  #example card :Red 7 , Green 8
    Colours = ["Red", "Blue", "Green", "Yellow"]
    values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "Draw two", "Skip", "Reverse"]
+   wilds = ["Wild", "Wild Draw Four"]
    for colour in Colours:
          for value in values:
               cardVal = " {} {}".format(colour, value)
               deck.append(cardVal)
               if value !=0:
                     deck.append(cardVal)
+   for i in range(4):
+        deck.append(wilds[0])
+        deck.append(wilds[1])   
    print(deck)
    return deck
 
